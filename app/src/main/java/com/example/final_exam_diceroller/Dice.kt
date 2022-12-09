e1aaf393e65e2f76b14d12bf2d0e431deef41753
toshly24 <113390335+toshly24@users.noreply.github.com>
2022-12-09 18:40:47 -0500

    Added the spinner and the dice.kt class

--- app/src/main/java/com/example/final_exam_diceroller/Dice.kt
+++ app/src/main/java/com/example/final_exam_diceroller/Dice.kt
@@ -1,5 +1,17 @@
 package com.example.final_exam_diceroller
 
-class Dice {
+class Dice (private val numSides: Int, private var sideUp:Int = 0) {
 
+    init {
+        roll()
+    }
+
+    fun getSide_up(): Int {
+        return sideUp
+    }
+
+    fun roll() //Roll method to generate a random value
+    {
+        sideUp = ((Math.random() * numSides) + 1).toInt()
+    }
 }
--- app/src/main/java/com/example/final_exam_diceroller/MainActivity.kt
+++ app/src/main/java/com/example/final_exam_diceroller/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.final_exam_diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import com.example.final_exam_diceroller.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+
+
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val spinneradapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item)
+        spinneradapter.add(getString(R.string.d4))
+        spinneradapter.add(getString(R.string.d6))
+        spinneradapter.add(getString(R.string.d8))
+        spinneradapter.add(getString(R.string.d10))
+        spinneradapter.add(getString(R.string.d12))
+        spinneradapter.add(getString(R.string.d20))
+
+        binding.sidesSpinner.adapter = spinneradapter
+
+        binding.sidesSpinner.onItemSelectedListener = this
     }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        when(p2)
+        {
+            0 ->
+            {
+
+            }
+        }
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        //TODO("Not yet implemented")
+    }
+
 }
