adae6e958d5dcd91996c19e9d25cac4104e88564
toshly24 <113390335+toshly24@users.noreply.github.com>
2022-12-09 22:19:23 -0500

    Added the code for the user input side and adding the side to the spinner

--- app/src/main/java/com/example/final_exam_diceroller/MainActivity.kt
+++ app/src/main/java/com/example/final_exam_diceroller/MainActivity.kt
@@ -6,15 +6,14 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.core.view.get
 import com.example.final_exam_diceroller.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
     var dice = Dice(4)
-
-    var result2 = 0
-    var result3 = 0
+    var value = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +21,13 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         setContentView(binding.root)
 
 
-        val spinneradapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item)
-        spinneradapter.add(getString(R.string.d4))
-        spinneradapter.add(getString(R.string.d6))
-        spinneradapter.add(getString(R.string.d8))
-        spinneradapter.add(getString(R.string.d10))
-        spinneradapter.add(getString(R.string.d12))
-        spinneradapter.add(getString(R.string.d20))
+        val spinneradapter = ArrayAdapter<Int>(this, android.R.layout.simple_spinner_item)
+        spinneradapter.add(4)
+        spinneradapter.add(6)
+        spinneradapter.add(8)
+        spinneradapter.add(10)
+        spinneradapter.add(12)
+        spinneradapter.add(20)
 
         binding.sidesSpinner.adapter = spinneradapter
 
@@ -44,38 +43,23 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             dice.roll()
             binding.result3.text = dice.getSide_up().toString()
         }
+
+
+        binding.add.setOnClickListener {
+            if (binding.sidesInput.text != null) {
+                spinneradapter.add((binding.sidesInput.text.toString()).toInt())
+                binding.sidesInput.text.clear()
+            }
+        }
     }
 
     override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-        when(p2)
-        {
-            0 ->
-            {
-               dice = Dice(4)
-            }
-            1 ->
-            {
-                dice = Dice(6)
-            }
-            2 ->
-            {
-                dice = Dice(8)
-            }
-            3 ->
-            {
-                dice = Dice(10)
-            }
-            4 ->
-            {
-                dice = Dice(12)
-            }
-            5 ->
-            {
-                dice = Dice(20)
-            }
 
-        }
-    }
+      var position = binding.sidesSpinner.getPositionForView(p1)
+        value = binding.sidesSpinner.getItemAtPosition(position).toString().toInt()
+                   dice = Dice(value)
+                }
+
 
     override fun onNothingSelected(p0: AdapterView<*>?) {
         //TODO("Not yet implemented")
